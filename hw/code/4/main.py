3e240f28d2af43ffb0dc07b770531c3a44e15cb7
Bennett Narron <bynarron@ncsu.edu>
2015-09-22 11:42:59 -0400

    Fixed print formatting

--- hw/code/4/main.py
+++ hw/code/4/main.py
@@ -6,26 +6,30 @@ Created on Fri Sep  4 11:28:18 2015
 """
 
 
-
+from __future__ import print_function
 import randomGeneration, energy
+
+def say(x): print(x, end="")
+
 #neighborVal = randomGeneration.getNeighbor()
 #print neighborVal
 minMaxSchaffer = energy.getBaselineMinMaxForSchaffer()
 ##############################
 kMaxVal =1000
 #kMinVal = 1
-eMaxVal = -2.0 ## as we have normalized it , it should be in between 0 and 1 
-###################### Start doing simulated annelaing 
+eMaxVal = -2.0 ## as we have normalized it , it should be in between 0 and 1
+###################### Start doing simulated annelaing
 startVal = randomGeneration.getRandVal()
 currEnergyVal = energy.calcNormEnergy(startVal, minMaxSchaffer[0], minMaxSchaffer[1])
-print "Starting Energy -> {} for {}".format(currEnergyVal, startVal)
-#### assignment 
+say("Starting Energy -> {} for {}\n".format(currEnergyVal, startVal))
+#### assignment
 currSolnVal=startVal
-bestSolVal = startVal 
+bestSolVal = startVal
 bestEnergyVal = currEnergyVal
 counter = 1000
 
 ###print purpose
+output = ""
 cntForQ=0
 cntForExcl=0
 cntForPlus=0
@@ -34,39 +38,40 @@ cntForDot=0
 
 while (counter > 0)  and (currEnergyVal > eMaxVal):
     #print "Inside : {}----{}".format(currEnergyVal, counter)
-    
+
     neighborVal = randomGeneration.getRandVal()
     energyNeighbour = energy.calcNormEnergy(neighborVal, minMaxSchaffer[0], minMaxSchaffer[1])
 
     if energyNeighbour < bestEnergyVal:
        bestEnergyVal = energyNeighbour
-       bestSolVal = neighborVal 
-       print "!" ,
+       bestSolVal = neighborVal
+       output +=  "!"
        cntForExcl = cntForExcl + 1
-    
+
     if energyNeighbour < currEnergyVal:
        currSolnVal = neighborVal
-       currEnergyVal = energyNeighbour 
-       print "+" , 
+       currEnergyVal = energyNeighbour
+       output +=  "+"
        cntForPlus = cntForPlus + 1
-    elif randomGeneration.getProbOfAcceptance(currEnergyVal, energyNeighbour, float(counter)/float(kMaxVal))  > randomGeneration.getRandomProb() :   
+    elif randomGeneration.getProbOfAcceptance(currEnergyVal, energyNeighbour, float(counter)/float(kMaxVal))  > randomGeneration.getRandomProb() :
        currSolnVal = neighborVal
-       currEnergyVal = energyNeighbour 
-       print "?" ,
+       currEnergyVal = energyNeighbour
+       output += "?"
        cntForQ = cntForQ + 1
-       
-    print "." , 
+
+    output +=  "."
     cntForDot = cntForDot + 1
     counter = counter - 1
     if counter % 40 == 0:
-       print "\n ?={} !={} +={} .={} |".format(cntForQ, cntForExcl, cntForPlus, cntForDot)
-       cntForQ = 0 
+       say("\n ?={} !={} +={} .={} | ".format(cntForQ, cntForExcl, cntForPlus, cntForDot))
+       say(output + '\n')
+       cntForQ = 0
        cntForExcl = 0
        cntForPlus = 0
        cntForDot = 0
+       output = ""
 
-print"\n"
 # print "We tried it {} times".format( counter)
-print"\n"
-print "The best solution is : {} , and it's energy is {}".format(bestSolVal, bestEnergyVal)        
-        
+say("\n")
+say("The best solution is : {} , and it's energy is {}\n".format(bestSolVal, bestEnergyVal))
+
