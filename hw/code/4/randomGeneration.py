b87ead5d91238cd42edb0c5ce84059958393aec5
Akond Rahman <akond.rahman.buet@gmail.com>
2015-09-26 21:13:11 -0400

    hw/code/4/ - major changes

--- hw/code/4/randomGeneration.py
+++ hw/code/4/randomGeneration.py
@@ -5,18 +5,41 @@ Created on Fri Sep  4 11:21:32 2015
 @author: akond
 """
 
+maxRange= 100000
+minRange=-100000
+
+def getNeighborRandonmly(neighborVec):
+  import math, random 
+  vecToRet=[]
+  maxRange=neighborVec[2]
+  minRange=neighborVec[0]
+  currentValP=neighborVec[1]
+  disFromMin=math.sqrt(math.pow( float(currentValP - minRange), 2))    
+  disFromMax=math.sqrt(math.pow( float(currentValP - maxRange), 2))   
+  #print "minDist = {}, maxDist={}".format(disFromMin, disFromMax)
+  disToLook = max(disFromMin, disFromMax)
+  randToRet = random.randrange(currentValP - disToLook, currentValP + disToLook)
+  #print  "rand to ret ....", randToRet
+  vecToRet = [currentValP - disToLook, randToRet , currentValP + disToLook]
+  return vecToRet
+
+def getRandVec():
+  import random 
+  randToret = 0 
+  randToret = random.randrange(minRange, maxRange)
+  vecToRet=[minRange, randToret ,maxRange]
+  return vecToRet
 
 
 def getRandVal():
   import random 
-  maxRange=100000
-  minRange=-100000
-  randToret = 0 
+  #maxRange=100000
+  #minRange=-100000
+  #randToret = 0 
   randToret = random.randrange(minRange, maxRange)
+  #vecToRet=[minRange, randToret ,maxRange]
   return randToret
-
-
-
+  
 def getProbOfAcceptance(currEnergyP, neighbourEnergyP, tParam):
   # t = k/kmax
   import math
@@ -29,4 +52,9 @@ def getProbOfAcceptance(currEnergyP, neighbourEnergyP, tParam):
 
 def getRandomProb():
    import random 
-   return random.random()       
+   return random.random()   
+
+
+
+
+#getNeighborRandonmly(-100)    
