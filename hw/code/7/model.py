d23d57ab28f1b392b6162b7062bb9959c7d62804
mrsingh <mrsingh@ncsu.edu>
2015-10-15 20:29:30 -0400

    Started adding Golinski model. initialization done. writing code for checking constraints

--- hw/code/7/model.py
+++ hw/code/7/model.py
@@ -43,6 +43,52 @@ class Model(object):
     def sumOfObjs(self):
         return sum(self.getobj())
 
+class Golinski(Model):
+
+    def __init__(self):
+        ## first specify the requirements         
+        self.decisionVec=[0,0,0,0,0,0,0]        
+        self.lowerRange=[2.6,0.7,17.0,7.3,7.3,2.9,5.0]
+        self.numOfDec=7
+        self.numOfObjs=10
+        self.upperRange=[3.6,0.8,28.0,8.3,8.3,3.9,5.5]
+        ## then create the initialization 
+        self.generateInitialVector()
+
+    def check(self):
+        constCheckerVec = [0,0,0,0,0,0,0]
+        constCheckerVec[0]=     ((1.0)/(self.decisionVec[0]*math.pow(self.decisionVec[1],2)*self.decisionVec[2])) - (1.0/27.0) 
+        constCheckerVec[1]= 6 - (self.decisionVec[0]) - (self.decisionVec[1])
+        constCheckerVec[2]= 2 - (self.decisionVec[1]) + (self.decisionVec[0])
+        constCheckerVec[3]= 2 - (self.decisionVec[0]) + (3*self.decisionVec[1])
+        constCheckerVec[4]= 4 - (self.decisionVec[3]) - math.pow(((self.decisionVec[2]) - 3), 2)
+        constCheckerVec[5]=     math.pow((self.decisionVec[4]-3), 3) + ((self.decisionVec[5]) - 4) 
+        for cnt in range(0,self.numOfDec):
+            if (self.decisionVec[cnt] < self.lowerRange[cnt]) or (self.decisionVec[cnt]>self.upperRange[cnt]) or (constCheckerVec[cnt] < 0):
+              return False
+        return True
+
+
+    def getobj(self):
+        giveSquaredValue = lambda val : math.pow(val, 2)
+        f1=-(
+             25 * math.pow((self.decisionVec[0]-2), 2)+
+             math.pow((self.decisionVec[1]-2), 2)     +
+             math.pow((self.decisionVec[2]-1), 2)     * 
+             math.pow((self.decisionVec[3]-4), 2)     + 
+             math.pow((self.decisionVec[4]-1), 2)
+             )
+        
+        f2=( 
+             giveSquaredValue(self.decisionVec[0]) + 
+             giveSquaredValue(self.decisionVec[1]) +
+             giveSquaredValue(self.decisionVec[2]) +
+             giveSquaredValue(self.decisionVec[3]) +
+             giveSquaredValue(self.decisionVec[4]) +
+             giveSquaredValue(self.decisionVec[5])
+           )
+        return [f1,f2]
+
 class Osyczka2(Model):
 
     def __init__(self):
